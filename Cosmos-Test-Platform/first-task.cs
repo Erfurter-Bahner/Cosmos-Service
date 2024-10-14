270087b0d7f71d7950b3a78b38d3a6dfc11c4b36
WasMachIchHier <lindemio0505@gmail.com>
2024-10-14 13:41:36 +0200

    Memory-Class

--- Cosmos-Test-Platform/first-task.cs
+++ Cosmos-Test-Platform/first-task.cs
@@ -11,6 +11,11 @@ namespace Cosmos_Test_Platform
 {
     public class Kernel : Sys.Kernel
     {
+
+
+        Memory mem= new Memory();
+
+
         DateTime start;
 
         protected override void BeforeRun()
@@ -21,12 +26,16 @@ namespace Cosmos_Test_Platform
 
         protected override void Run()
         {
+
+
             Console.Write("Input: ");
             var input = Console.ReadLine();
             Console.Write("Text typed : ");
             string[] args = input.Split(' ');
             Console.WriteLine(input);
-            
+
+            //System.IO.File.WriteAllText(@"C:\Desktop\test.txt", input);
+
             switch (args[0])
             {
                 case "help":
@@ -39,6 +48,20 @@ namespace Cosmos_Test_Platform
                         getSysTime();
                         break;
                 }
+                case "write":
+                {
+                        mem.Schreiben(uint.Parse(args[1]), byte.Parse(args[2]));
+                        break;
+                }
+
+                case "read":
+                    {
+                        int a= mem.Lesen(uint.Parse(args[1]));
+                        Console.WriteLine(a);
+                        
+                        break;
+                    }
+
                 default: Console.WriteLine("eingabe falsch");
                     break; 
             }
@@ -50,6 +73,27 @@ namespace Cosmos_Test_Platform
             TimeSpan runtime = DateTime.Now - start;
             Console.WriteLine("running for: " + runtime.TotalSeconds + " seconds");
 
+        }
+
+
+
+
+
+    }
+
+
+    class Memory
+    {
+        Cosmos.Core.ManagedMemoryBlock newBlock = new Cosmos.Core.ManagedMemoryBlock(16);
+        public void Schreiben(uint index, byte value)
+        {
+            newBlock.Write8(index, value);
+        }
+        public ushort Lesen(uint index)
+        {
+            ushort val = 0;
+            val = newBlock.Read16(index);
+            return (byte)(val & 0xFF);
         }
 
 
