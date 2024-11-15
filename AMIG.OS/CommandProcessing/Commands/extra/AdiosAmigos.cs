4fd04fe0e0e42f7e690c3921b043d5f34ddf17c9
Steven Rötschke <legend.of.xsar@gmail.com>
2024-11-15 18:02:41 +0100

    added shutdown cmd - derzeit noch nicht ausführbar

---
+++ AMIG.OS/CommandProcessing/Commands/extra/AdiosAmigos.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Contracts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Sys = Cosmos.System;
+
+using AMIG.OS.UserSystemManagement;
+using AMIG.OS.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AMIG.OS.CommandProcessing.Commands.extra
+{
+    public class Adios : ICommand
+    {
+        private readonly UserManagement userManagement;
+        public string Description => "shutdown the system";
+        public string PermissionName { get; } = "NormalCmd"; // Required permission name
+        public Dictionary<string, string> Parameters => new Dictionary<string, string>
+        {
+            {"-amigos", "needed to use shutdown command."},
+        };
+
+        // Implementing CanExecute as defined in the custom ICommand interface
+        public bool CanExecute(User currentUser)
+        {
+            return currentUser.HasPermission(PermissionName);
+        }
+
+        // Implementing Execute as defined in the custom ICommand interface
+        public void Execute(string[] args, User currendUser)
+        {
+            if (args.Contains("-help"))
+            {
+                ShowHelp();
+                return;
+            }
+            if (args.Length == 2 && args[1].Equals("amigos"))
+            {
+                Console.Clear();
+                Console.WriteLine("\n\tHASTA LA VISTA");
+                Thread.Sleep(1500);
+                Sys.Power.Shutdown();
+            }
+            else
+            {
+                Console.WriteLine("Insufficient arguments. Use -help to see usage.");
+            }
+        }
+
+        // Show help method as defined in the custom ICommand interface
+        public void ShowHelp()
+        {
+            Console.WriteLine(Description);
+            Console.WriteLine("Usage: adios [options]");
+            foreach (var param in Parameters)
+            {
+                Console.WriteLine($"  {param.Key}\t{param.Value}");
+            }
+        }
+    }
+}
